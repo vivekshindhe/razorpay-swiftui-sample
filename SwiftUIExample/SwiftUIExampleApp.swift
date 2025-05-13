c88873b73acdc79baa640d990099876e43b00c98
Vivek Shindhe <vivek.shindhe@razorpay.com>
2025-05-13 14:43:26 +0530

    Initial Commit

---
+++ SwiftUIExample/SwiftUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExampleApp.swift
+//  SwiftUIExample
+//
+//  Created by Vivek Rajesh Shindhe on 13/05/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIExampleTests/SwiftUIExampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExampleTests.swift
+//  SwiftUIExampleTests
+//
+//  Created by Vivek Rajesh Shindhe on 13/05/25.
+//
+
+import Testing
+@testable import SwiftUIExample
+
+struct SwiftUIExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
